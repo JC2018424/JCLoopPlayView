96d7a9b2e4ad02735c6d7a97938e3e5d325dc148
JC2018424 <13451001517@163.com>
2018-08-02 17:16:09 +0800

    支持本地图片，url地址
    增加新功能

--- Example/JCLoopPlayView/ViewController.swift
+++ Example/JCLoopPlayView/ViewController.swift
@@ -11,14 +11,18 @@ import UIKit
 class ViewController: UIViewController, JCLoopPlayDelegate {
 
     private let loopView: JCLoopPlayView = {
-        let imageStrs = ["cat", "book", "cup", "orange", "desk", "fllow"]
+        let imageStrs = ["cat", "http://img.hb.aicdn.com/e7c668c293eaf44165c254b059ea991a765c525f70bb3d-GcUann_fw658", "cup", "orange", "desk", "fllow"]
         let rect = CGRect(x: 0, y: 20, width: UIScreen.main.bounds.width, height: 300)
         let loopView = JCLoopPlayView(frame: rect, imageStrs: imageStrs)
         return loopView
     }()
     
     public func currentIndex(_ index: Int) {
-        print("index = \(index - 1)")
+        print("当前index = \(index - 1)")
+    }
+    
+    public func selectedIndex(_ index: Int) {
+        print("选中index = \(index - 1)")
     }
     
     override func viewDidLoad() {
